943882ffd348f9217186017bcce7499a24a521eb
Hui Wang <huiw3939@gmail.com>
2018-03-06 17:47:28 -0500

    hoisting

---
+++ globalExecutionContext/index.js
@@ -0,0 +1,10 @@
+//hoisting
+test()
+var variable = "var1"
+//if you check the console variable is defined and anothervar is not.
+debugger
+function test(){
+  console.log("hello")
+}
+
+var anotherVar = "var2"
